8d275610f41a5617e36dc74833a89bb04b24ace3
Kelly Smith <k.smith041010@gmail.com>
2026-04-21 16:40:31 -0500

    Update Guessing_Game.py
    Upgrade game logic with a feedback system

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,30 +1,57 @@
+playing = True
 import random
 
-secret = random.randint(18,50)
-print("DEBUG:", secret)
-
-while True: # <- keeps the whole game running
-    for i in range(3):
+while playing:
+    secret = random.randint(0,50)
+    print("DEBUG:", secret)
+    previous_distance = None
+    for i in range(5):
         print("Attempt", i + 1)
     
-        guess = int(input("You only have 3 tries. Make them count. Guess a number between 0 and 20: "))
+        guess = int(input("You have 5 attempts to guess the secret number. Make them count. Guess a number between 0 and 50: "))
     
         if guess == secret:
-            print("Whoop whoop!")
+            print("Whoop whoop! You got it!")
             break
         else:
-            print("Sad face")
+            distance = abs(secret - guess)
+            
+            # First feedback (hot/cold)
+            if distance <= 3:
+                print("🔥 YOUR BURNING UP!")
+            elif distance <= 7:
+                print("🌡 You're warm!")
+            elif distance <=15:
+                print("🧊 You're cold.")
+            else:
+                print("🥶  Brr! You're freezing")
+            
+            # Hotter / colder tracking
+            if previous_distance is not None:
+                if distance < previous_distance:
+                    print("📈 You're getting hotter!")
+                elif distance > previous_distance:
+                    print("📉 You're getting colder.")
+                else:
+                    print("➡️You're the same distance as before.")
+                    
+            # update tracker
+            previous_distance = distance
     else:
         print("Out of tries")
     
-    #NEW PART
-    again = input("Would you like to try again? (Yes/No): ")
-
-    if again.lower() == "no":
-        print("Game over. À bientôt!")
-        break
-
+    while True:
+        again = input("Would you like to try again? (Y/N): ").strip().lower()
     
-
-              
+        if again == "y":
+            break # go back to top of main loop
+    
+        elif again == "n":
+            print("Game over. À bientôt!")
+            playing = False
+            break
+        
+        else:
+            print("Please enter only Y or N.")
+            break
         
